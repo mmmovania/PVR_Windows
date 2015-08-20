6d4b1c2937de0dd57d8420d235417a5d00f77daa
Muhammad Mobeen Movania <mova0002@e.ntu.edu.sg>
2015-08-20 13:08:56 +0500

    Removed redundant files.
    Removed redundant files

--- pvrbook.bat
+++ pvrbook.bat
@@ -1,3 +1,1 @@
-rem  the THIRD_PARTY_TOOLS_HOME environment variable defines where dependencies (includes, libs) for PVR are stored
-set THIRD_PARTY_TOOLS_HOME=d:\Libraries\PVR\3rdParty
 vs2010\pvrbook.sln
