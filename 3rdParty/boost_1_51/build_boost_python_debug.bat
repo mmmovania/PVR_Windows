f89e298e8222710e929538b6949eeabb507a69df
Muhammad Mobeen Movania <mova0002@e.ntu.edu.sg>
2015-08-24 12:26:47 +0500

    Updated boost bat files
    Updated boost bat files

--- 3rdParty/boost_1_51/build_boost_python_debug.bat
+++ 3rdParty/boost_1_51/build_boost_python_debug.bat
@@ -1,12 +1,1 @@
-@ECHO OFF
-IF "%PYTHONHOME%" == "" GOTO NOPATH
-
-:YESPATH
-@ECHO The PYTHONHOME environment variable was detected.
 bjam.exe toolset=msvc-10.0 --user-config=user-config.jam --with-python python-debugging=on threading=multi variant=debug link=shared stage
-GOTO END
-
-:NOPATH
-@ECHO The PYTHONHOME environment variable was NOT detected. Defaulting to C:\Python27
-bjam.exe toolset=msvc-10.0 --user-config=user-config-default.jam --with-python python-debugging=on threading=multi variant=debug link=shared stage
-:END
